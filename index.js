60ddfae5934e31299f502fc5135573a7a1dcab49
Peter Moresi <peter.moresi@gmail.com>
2015-09-12 15:37:18 -0700

    v1.0.1

--- index.js
+++ index.js
@@ -24,10 +24,6 @@
             lookup_value = arguments[0].valueOf();
             lookup_array = arguments[1];
 
-            if (fn.ISRANGE(lookup_array)) {
-                lookup_array = lookup_array.valueOf();
-            }
-
             for (var i = 0; i < lookup_array.length; i++) {
                 if (typeof lookup_array[i] !== 'undefined' && lookup_value === lookup_array[i].valueOf()) {
                     return lookup_array[i];
@@ -38,14 +34,6 @@
             lookup_value = arguments[0].valueOf();
             lookup_vector = arguments[1];
             results_vector = arguments[2];
-
-            if (fn.ISRANGE(lookup_vector)) {
-                lookup_vector = lookup_vector.valueOf();
-            }
-
-            if (fn.ISRANGE(results_vector)) {
-                results_vector = results_vector.valueOf();
-            }
 
             for (var i = 0; i < lookup_vector.length; i++) {
                 if (typeof lookup_vector[i] !== 'undefined' && lookup_value === lookup_vector[i].valueOf()) {
